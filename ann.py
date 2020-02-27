1989eba2d7f4ee1e3169fe392df8a1b469f33d63
Bayu Saputra <babahayoo@gmail.com>
2020-02-27 16:59:31 +0700

    adding some library, dataset, encode categorical data and splitting

---
+++ ann.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+#part 1 - data preprocessing
+
+#step 1 - import library
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#step 2 - import dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+
